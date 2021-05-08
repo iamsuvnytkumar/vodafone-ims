8a1bd3ca4a64c709ad4b93f73d69a398cd25955c
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-09 02:41:02 +1200

    Update server.php

--- server.php
+++ server.php
@@ -9,9 +9,9 @@ $errors = array();
 
 // connect to the database
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["b8fde6f3e15a94"];
+$cleardb_password = $cleardb_url["9e0c4460"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
