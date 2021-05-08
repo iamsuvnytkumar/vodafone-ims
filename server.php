5932a0e98b7c611b62e1742e464efc042110f0f6
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-09 03:29:04 +1200

    Update server.php

--- server.php
+++ server.php
@@ -7,8 +7,19 @@ $email    = "";
 
 $errors = array(); 
 
-// connect to the database
-$db = mysqli_connect('us-cdbr-east-03.cleardb.com', 'b8fde6f3e15a94', '9e0c4460', 'heroku_1fc3bbe61793651');
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-03.cleardb.com"];
+$cleardb_username = $cleardb_url["b8fde6f3e15a94"];
+$cleardb_password = $cleardb_url["9e0c4460"];
+$cleardb_db = substr($cleardb_url["heroku_1fc3bbe61793651"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
+
+
 if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
