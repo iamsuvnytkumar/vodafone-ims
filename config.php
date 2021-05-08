bd94dd7450daeeb5576c9e8f7d3c80c4317fa381
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-09 04:04:29 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('us-cdbr-east-03.cleardb.com', 'b8fde6f3e15a94', '9e0c4460', 'heroku_1fc3bbe61793651');
+$db = mysqli_connect('remotemysql.com', 'R6FJDzfo0P', 'T8ze1v3QPx', 'R6FJDzfo0P');
 if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
