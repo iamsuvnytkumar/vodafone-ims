4178d5ca85bb9e56e8d5e8b9b9863386b2c0e0d2
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-09 16:26:36 +1200

    Update additem.php

--- additem.php
+++ additem.php
@@ -19,8 +19,7 @@ $item_name = "";
 $item_price    = "";
 
 
-// connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'inventorymanagement');
+$db = mysqli_connect('us-cdbr-east-03.cleardb.com', 'b8fde6f3e15a94', '9e0c4460', 'heroku_1fc3bbe61793651');
 if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
