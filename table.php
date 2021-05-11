5141e817905d64ac04f9218596d099d5e42f2ef6
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 06:10:10 +1200

    Update table.php

--- table.php
+++ table.php
@@ -20,7 +20,7 @@ if (isset($_GET['logout'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Inventory</title>
     <link rel="shortcut icon" type="image/jpg" href="favicon.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
