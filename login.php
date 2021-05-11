6aaf3c607d8a1eea65aaf7f5edafaf3bf6d9c671
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 05:31:26 +1200

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,8 @@
 <html lang="en">
 
 <head>
+    <title>Login</title>
+    <link rel="shortcut icon" type="image/jpg" href="logo.png"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
