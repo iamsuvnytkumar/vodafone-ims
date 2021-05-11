05cbae21a390c8106a43457ee8c84225af19f4fa
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 01:18:43 +1200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -98,7 +98,7 @@ if (isset($_GET['logout'])) {
         <div class="sidebar-menu">
             <div class="sidebar-header">
                 <div class="logo">
-                    <a href="dashboard.php"><img src="assets/images/icon/logo.PNG" alt="logo"></a>
+                    <a href="dashboard.php"><img src="assets/images/icon/logo.png" alt="logo"></a>
                 </div>
             </div>
             <div class="main-menu">
