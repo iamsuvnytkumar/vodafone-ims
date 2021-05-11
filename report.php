7c16d0866cf8b1bf20d6b277585b9894cd878e04
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 05:57:34 +1200

    Update report.php

--- report.php
+++ report.php
@@ -21,7 +21,7 @@ if (isset($_GET['logout'])) {
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Reports</title>
-    <link rel="shortcut icon" type="image/jpg" href="logo.png"/>
+    <link rel="shortcut icon" type="image/jpg" href="favicon.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
