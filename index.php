76833dc52d79cfc46e4b13d32b99ff7ec1b1b37a
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 05:56:53 +1200

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_GET['logout'])) {
 
 <head>
     <title>Home</title>
-    <link rel="shortcut icon" type="image/jpg" href="logo.png"/>
+    <link rel="shortcut icon" type="image/jpg" href="favicon.png"/>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="Inventmng/srtdash/assets/bootstrap.min.css">
 </head>
