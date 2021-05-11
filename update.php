8c835d60609948d467ce490c9f777b400f23af49
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 05:55:53 +1200

    Update update.php

--- update.php
+++ update.php
@@ -42,7 +42,7 @@ echo "No results!";
 <html>
 <head>
 <title>Update</title>
-<link rel="shortcut icon" type="image/jpg" href="logo.png"/>
+<link rel="shortcut icon" type="image/jpg" href="favicon.png"/>
   
       <style>
         .content {
