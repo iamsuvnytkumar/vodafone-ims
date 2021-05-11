676bf2478955c518bba1c112bf7586e4161106bd
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 02:00:56 +1200

    Update report.php

--- report.php
+++ report.php
@@ -59,7 +59,7 @@ if (isset($_GET['logout'])) {
         .card{
             margin-left: auto;
             margin-right: auto;
-            width: 800px;
+            width: auto;
         }
         .form-group{
             width: 700px;
@@ -83,7 +83,7 @@ if (isset($_GET['logout'])) {
         <div class="sidebar-menu">
             <div class="sidebar-header">
                 <div class="logo">
-                    <a href="index.php"><img src="assets/images/icon/logo.png" alt="logo"></a>
+                    <a href="index.php"><img src="logo.png" alt="logo"></a>
                 </div>
             </div>
             <div class="main-menu">
