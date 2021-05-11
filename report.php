e8eb607b5c2b22d751b7997d1a32a0c510f05245
iamsuvnytkumar <83863844+iamsuvnytkumar@users.noreply.github.com>
2021-05-12 05:34:15 +1200

    Update report.php

--- report.php
+++ report.php
@@ -20,7 +20,8 @@ if (isset($_GET['logout'])) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Inventory Management System</title>
+    <title>Reports</title>
+    <link rel="shortcut icon" type="image/jpg" href="logo.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
